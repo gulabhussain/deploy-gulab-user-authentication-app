a8e50668cdfe2bb1a623f44d85b4cef808dd22a5
Gulab Hussain <gulabhussain2033@gmail.com>
2026-01-28 20:24:35 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -27,7 +27,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = "http://localhost:5000/auth/login";
+            const url = `https://deploy-gulab-user-authentication-ap.vercel.app/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: { 'Content-Type': 'application/json' },
