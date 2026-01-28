4247f2749c805030208ae155a6653a3496332743
Gulab Hussain <gulabhussain2033@gmail.com>
2026-01-28 20:25:19 +0530

    Update API URL to production endpoint

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
     const fetchProducts = async () => {
         try {
-            const url = "http://localhost:3000/products";
+            const url = "https://deploy-gulab-user-authentication-ap.vercel.app/products";
             const headers = {
                 headers: {
                     'Authorization': localStorage.getItem('token')
