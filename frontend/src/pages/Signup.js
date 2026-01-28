d6e303f13cf48fed26466403d2bdd242dd354c17
Gulab Hussain <gulabhussain2033@gmail.com>
2026-01-28 20:23:40 +0530

    Update signup URL to production endpoint

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -28,7 +28,7 @@ function Signup() {
             return handleError('name, email and password are required')
         }
         try {
-            const url = "http://localhost:5000/auth/signup";
+            const url = `https://deploy-gulab-user-authentication-ap.vercel.app/auth/signup`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: { 'Content-Type': 'application/json' },
